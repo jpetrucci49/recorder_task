7c64338d5c1d9dfeac4bf01dda981e3af7b048eb
Joseph Petrucci <jpetrucci49@yahoo.com>
2024-08-21 09:28:36 -0400

    Add textarea and radio buttons with basic functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
+import React, { useState } from 'react';
 import CustomButton from './components/CustomButton';
+
 import './App.css';
 
 function App() {
+  const [selectedRadio, setSelectedRadio] = useState(false)
+
   return (
     <div className="App">
       <h4>Say the vocabulary words.</h4>
-      <div className="button-wrapper">
-      <CustomButton symbol="square" color="#faaf90"/>
-      <CustomButton symbol="circle" color="#69d06d"/>
-      <CustomButton symbol="play" color="#ac2020"/>
+      <div className="button-container">
+        <CustomButton symbol="square" color="#faaf90"/>
+        <CustomButton symbol="circle" color="#69d06d"/>
+        <CustomButton symbol="play" color="#ac2020"/>
+      </div>
+      <div>
+        <textarea
+          id="freeform"
+          name="freeformInput"
+          rows={5}
+          cols={60}
+        />
+      </div>
+      <div>
+        <form className="radio-form">
+          <div className="radio">
+            <label>
+              <input type="radio" value={true} checked={selectedRadio === true} onClick={()=>setSelectedRadio(true)}/>
+              True
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" value={false} checked={selectedRadio === false} onClick={()=>setSelectedRadio(false)}/>
+              False
+            </label>
+          </div>
+          <button disabled={!selectedRadio} type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const CustomButton = (props) => {
-
+    const labelObj = {
+        square: "Stop",
+        circle: "Record",
+        play: "Review your recording"
+    }
     return (
-        <div className="button" style={{backgroundColor: props.color}}>
-            <div className={props.symbol}></div>
+        <div className="button-wrapper">
+            <div className="button" style={{backgroundColor: props.color}}>
+                <div className={props.symbol}></div>
+            </div>
+            <label>{labelObj[props.symbol]}</label>
         </div>
     )
 }
