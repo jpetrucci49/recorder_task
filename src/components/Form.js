faa00453aa381beb62675c04dd71bda0cc7a2475
Joseph Petrucci <jpetrucci49@yahoo.com>
2024-08-21 10:06:37 -0400

    Refactored radio form, added final answer prompt

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,18 +13,15 @@ const Form = (props) => {
                 cols={60}
                 />
             </div>
+            <div>Is this your final answer?</div>
             <div className="radio-buttons">
                 <div className="radio">
-                    <label>
-                    <input type="radio" value={true} checked={selectedRadio === true} onClick={()=>setSelectedRadio(true)}/>
-                    True
-                    </label>
+                    <input id="true" type="radio" value={true} checked={selectedRadio === true} onClick={()=>setSelectedRadio(true)}/>
+                    <label htmlFor="true">True</label>
                 </div>
                 <div className="radio">
-                    <label>
-                    <input type="radio" value={false} checked={selectedRadio === false} onClick={()=>setSelectedRadio(false)}/>
-                    False
-                    </label>
+                    <input id="false" type="radio" value={false} checked={selectedRadio === false} onClick={()=>setSelectedRadio(false)}/>
+                    <label htmlFor="false">False</label>
                 </div>
                 <button disabled={!selectedRadio} type="submit">Submit</button>
             </div>
